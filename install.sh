a1ef1ec5b50dc21784b0291d4881a1f55a83fbf6
sys0wn <138518220+sys0wn@users.noreply.github.com>
2024-03-08 17:03:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@ script_dir=$(realpath $(dirname $0))
 
 sudo cp $script_dir/scripts /opt/subcollect/ -r
 sudo cp $script_dir/subcollect.sh /usr/bin/subcollect
+
+if [ -n $(which amass) ]; then
+    sudo apt install amass
+fi
