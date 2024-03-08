50d7ef1fceebeee32884c327dd3f9e33910b609e
sys0wn <138518220+sys0wn@users.noreply.github.com>
2024-03-08 17:26:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,26 @@ script_dir=$(realpath $(dirname $0))
 sudo cp $script_dir/scripts /opt/subcollect/ -r
 sudo cp $script_dir/subcollect.sh /usr/bin/subcollect
 
-if [ -n "$(which amass)" ]; then
+
+if [ "$(which amass)" == "amass not found" ]; then
     sudo apt install amass
+fi
+
+if [ "$(which puredns)" == "puredns not found" ]; then
+    if ["$(which go)" == "go not found"]; then
+       sudo apt install -y golang 
+    git clone https://github.com/blechschmidt/massdns.git
+    cd massdns
+    make
+    sudo make install
+    go install github.com/d3mondev/puredns/v2@latest
+    sudo mv ~/go/bin/puredns /usr/bin
+    curl  https://raw.githubusercontent.com/trickest/resolvers/main/resolvers-trusted.txt > ~/.config/puredns/resolvers.txt
+fi
+
+if [ "$(which gotator)" == "gotator not found" ]; then
+    if ["$(which go)" == "go not found"]; then
+       sudo apt install -y golang 
+    go install github.com/Josue87/gotator@latest
+    sudo mv ~/go/bin/gotator /usr/bin
 fi
