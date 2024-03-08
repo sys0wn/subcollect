fcc753c521a16bf2ee053839997874ccfc148eae
sys0wn <138518220+sys0wn@users.noreply.github.com>
2024-03-08 16:26:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 sudo mkdir -p /opt/subcollect
 script_dir=$(realpath $(dirname $0))
 
-cp $script_dir/scripts /opt/subcollect/
-cp $script_dir/ /opt/subcollect/
+sudo $script_dir/scripts /opt/subcollect/ -r
