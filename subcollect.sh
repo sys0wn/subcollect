86806bd30c8809e63afcca94e4d3b706084a54ca
sys0wn <138518220+sys0wn@users.noreply.github.com>
2024-03-09 15:41:28 +0100

    Update subcollect.sh

--- subcollect.sh
+++ subcollect.sh
@@ -1,7 +1,7 @@
 folderName=$1Subcollect
 
 
-echo "\n\nCreating Project Folder in Current Working Directoryn\n\n"
+echo "\n\nCreating Project Folder in Current Working Directoryn:\n"
 
 echo -n $folderName | sudo tee /opt/subcollect/folderNameTransfer.txt
 
