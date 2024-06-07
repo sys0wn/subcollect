e50cce9fb8c5fca56f0bf7f6d172f5242728f6a5
sys0wn <138518220+sys0wn@users.noreply.github.com>
2024-06-07 14:05:49 +0200

    Update amassOutputFilterIrrelevant.py

--- scripts/amassOutputFilterIrrelevant.py
+++ scripts/amassOutputFilterIrrelevant.py
@@ -5,10 +5,26 @@ targetDomain = sys.argv[2]
 
 filteredSubdomainArray = []
 
+targetDomainArray = targetDomain.split(".")
+
+OnlyReleventDomainsRegex = ""
+
+#Dynamic regex builder
+
+for i in range(len(targetDomainArray) - 1):
+    OnlyReleventDomainsRegex += targetDomainArray[i] + "\."
+
+    if( i == len(targetDomainArray) - 1):
+        OnlyReleventDomainsRegex = OnlyReleventDomainsRegex[:-2]
+        OnlyReleventDomainsRegex += "$"
+
+# Write changes
+
 with open(inputFile, "r") as file:
     for line in file:
-        if targetDomain in line:
-            filteredSubdomainArray.append(line)
+        filteredDomains = re.findall(OnlyReleventDomainsRegex, line)
+        for goodDomain in filteredDomains:
+            filteredSubdomainArray.append(goodDomain)
 
 
 #clear file
